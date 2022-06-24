9c2f02dbc0d365f0d293049fbf7e671cfd89355a
Henry_DelGlitch <CodingWHenry@gmail.com>
2022-06-24 19:44:23 -0400

    Created content excerpt and content none templates

--- index.php
+++ index.php
@@ -4,57 +4,23 @@
 ?>
 
 <div class="content-area">
-<div class="container">
-    <div class="row">
-    <div class="col-md-8 offset-md-2 overflow-hidden">
-
-        <article class="post">
-        <div class="meta">
-            <span>Sep 24, 2021</span>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2 overflow-hidden">
+                <?php
+                    //Are there any posts in the DB?
+                    if( have_posts() ) {
+                        while( have_posts() ) {
+                            the_post();
+                            get_template_part( 'template-parts/content', 'excerpt' );
+                        }
+                    } else {
+                        get_template_part( 'template-parts/content', 'none' );
+                    }
+                ?>
+            </div>
         </div>
-        <h2><a href="#">Blog post title</a></h2>
-        <p class="excerpt">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis expedita molestias id, dolorem sapiente quae dolores modi hic et ea, suscipit minima eligendi magnam odio reiciendis dolorum, non placeat?
-        </p>
-        <a href="#" class="read-more">Read the full post -&gt;</a>
-        </article>
-
-        <article class="post my-5">
-        <div class="meta">
-            <span>Sep 24, 2021</span>
-        </div>
-        <h2><a href="#">Blog post title</a></h2>
-        <p class="excerpt">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis expedita molestias id, dolorem sapiente quae dolores modi hic et ea, suscipit minima eligendi magnam odio reiciendis dolorum, non placeat?
-        </p>
-        <a href="#" class="read-more">Read the full post -&gt;</a>
-        </article>
-
-        <article class="post my-5">
-        <div class="meta">
-            <span>Sep 24, 2021</span>
-        </div>
-        <h2><a href="#">Blog post title</a></h2>
-        <p class="excerpt">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis expedita molestias id, dolorem sapiente quae dolores modi hic et ea, suscipit minima eligendi magnam odio reiciendis dolorum, non placeat?
-        </p>
-        <a href="#" class="read-more">Read the full post -&gt;</a>
-        </article>
-
-        <article class="post my-5">
-        <div class="meta">
-            <span>Sep 24, 2021</span>
-        </div>
-        <h2><a href="#">Blog post title</a></h2>
-        <p class="excerpt">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis expedita molestias id, dolorem sapiente quae dolores modi hic et ea, suscipit minima eligendi magnam odio reiciendis dolorum, non placeat?
-        </p>
-        <a href="#" class="read-more">Read the full post -&gt;</a>
-        </article>
-
     </div>
-    </div>
-</div>
 </div>
 
 <?php get_footer(); ?>
