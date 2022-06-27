d47e0110cb03d7da29159e6e6e664218797c1a3a
Henry_DelGlitch <CodingWHenry@gmail.com>
2022-06-27 12:10:28 -0400

    Made the footer dynamic

--- footer.php
+++ footer.php
@@ -1,16 +1,24 @@
 <footer>
+
+<?php if ( get_theme_mod( 'footer_calltoaction_visbility' ) ) {?>
+
 <div class="footer-calltoaction text-center">
     <div class="container">
     <div class="row">
         <div class="col-md-8 offset-md-2 overflow-hidden">
-        <p class="sub-title">Join the course</p>
-        <h2>Bootstrap to WordPress 2.0</h2>
-        <p>Learn how to design and build custom, beautiful & responsive WordPress websites and themes for beginners in 2021 and beyond!</p>
-        <a href="#" class="btn btn-primary">Join now -&gt;</a>
+        <p class="sub-title"><?php  echo esc_html__( get_theme_mod( 'footer_sub_heading', 'Join the course' ) )?></p>
+        <h2><?php  echo esc_html__( get_theme_mod( 'footer_calltoaction_heading', 'Bootstrap to WordPress' ) )?></h2>
+        <p><?php  echo esc_html__( get_theme_mod( 'footer_calltoaction_desc', 'Learn how to design and build custom,
+        beautiful & responsive WordPress websites and themes for beginners in 2022 and beyond!' ) )?></p>
+        <a href="<?php  echo esc_html__( get_theme_mod( 'footer_cta_link', '#' ) )?>" class="btn btn-primary">
+        <?php  echo esc_html__( get_theme_mod( 'footer_calltoaction_button', 'Join now ->' ) )?></a>
         </div>
     </div>
     </div>
 </div>
+
+<? } ?>
+
 <div class="copyright text-center">
     <p><?php echo wp_kses_post( get_theme_mod( 'footer_copyright', 'Copyright Henry DelGlitch')) ?></p>
 </div>
--- includes/customizer-b2w.php
+++ includes/customizer-b2w.php
@@ -83,3 +83,116 @@ new \Kirki\Field\Textarea(
         ),
     ]
 );
+
+// Section -- Pre-Footer CTA
+
+new \Kirki\Section (
+    'footer_calltoaction_section',
+    [
+        'title'             => esc_html__( 'Call to Action', 'bootstrap2wordpress' ),
+        'description'       => esc_html__( 'This is the Call to Action Card, in the Pre-Footer Section', 
+        'bootstrap2wordpress' ),
+        'panel'             => 'b2w_theme_option_panel',
+        'priority'          => 160,
+    ]
+);
+
+// Section -- Pre-Footer CTA -- Fields
+
+new \Kirki\Field\Checkbox_Switch(
+    [
+        'settings'          => 'footer_calltoaction_visibility',
+        'label'             => esc_html__( 'Call to Action Section', 'bootstrap2wordpress' ),
+        'description'       => esc_html__( 'Toggle this section on or off.', 
+        'bootstrap2wordpress' ),
+        'section'           => 'footer_calltoaction_section',
+        'default'           => 'on',
+        'choices'           => [
+            'on'            => esc_html__( 'Enable', 'bootstrap2wordpress' ),
+            'off'           => esc_html__( 'Disable', 'bootstrap2wordpress' )
+        ] 
+    ]
+);
+
+new \Kirki\Field\Text(
+    [
+        'settings'          => 'footer_sub_heading',
+        'label'             => esc_html__( 'Sub Heading', 'bootstrap2wordpress' ),
+        'tooltip'           => esc_html__( 'Call to Action Section Sub Heading Text', 'bootstrap2wordpress' ),
+        'section'           => 'footer_calltoaction_section',
+        'default'           => esc_html__( 'Join the course', 'bootstrap2wordpress'),
+        'partial_refresh'   => array(
+            'footer_sub_heading'  => array(
+                'selector'        => 'footer-calltoaction p.sub-title',
+                'render_callback' => function() {
+                    return get_theme_mod( 'footer_sub_heading' );
+                }
+            ),
+        ),
+    ]
+);
+
+new \Kirki\Field\Text(
+    [
+        'settings'          => 'footer_calltoaction_heading',
+        'label'             => esc_html__( 'Heading', 'bootstrap2wordpress' ),
+        'tooltip'           => esc_html__( 'Call to Action Section Heading Text', 'bootstrap2wordpress' ),
+        'section'           => 'footer_calltoaction_section',
+        'default'           => esc_html__( 'Bootstrap to WordPress', 'bootstrap2wordpress'),
+        'partial_refresh'   => array(
+            'footer_calltoaction_heading'  => array(
+                'selector'        => 'footer .footer-calltoaction h2',
+                'render_callback' => function() {
+                    return get_theme_mod( 'footer_calltoaction_heading' );
+                }
+            ),
+        ),
+    ]
+);
+
+new \Kirki\Field\Textarea(
+    [
+        'settings'          => 'footer_calltoaction_desc',
+        'label'             => esc_html__( 'Description', 'bootstrap2wordpress' ),
+        'section'           => 'footer_calltoaction_section',
+        'default'           => esc_html__( 'Learn how to design and build custom, beautiful & responsive WordPress
+        websites and themes for beginners in 2022', 'bootstrap2wordpress'),
+        'partial_refresh'   => array(
+            'footer_calltoaction_desc'  => array(
+                'selector'        => '.footer-calltoaction p.fcta-desc',
+                'render_callback' => function() {
+                    return get_theme_mod( 'footer_calltoaction_desc' );
+                }
+            ),
+        ),
+    ]
+);
+
+new \Kirki\Field\Text(
+    [
+        'settings'          => 'footer_calltoaction_button',
+        'label'             => esc_html__( 'Button Text', 'bootstrap2wordpress' ),
+        'section'           => 'footer_calltoaction_section',
+        'default'           => esc_html__( 'Join now ->'),
+        'priority'          => 10,
+        'partial_refresh'   => array(
+            'footer_calltoaction_button'  => array(
+                'selector'        => '.footer-calltoaction .btn',
+                'render_callback' => function() {
+                    return get_theme_mod( 'footer_calltoaction_button' );
+                }
+            ),
+        ),
+    ]
+);
+
+new \Kirki\Field\URL(
+    [
+        'settings'          => 'footer_cta_link',
+        'label'             => esc_html__( 'Button Link', 'bootstrap2wordpress' ),
+        'tooltip'           => esc_html__( 'Button URL Goes Here', 'bootstrap2wordpress' ),
+        'section'           => 'footer_calltoaction_section',
+        'default'           => '#',
+        'priority'          => 10,
+    ]
+);
